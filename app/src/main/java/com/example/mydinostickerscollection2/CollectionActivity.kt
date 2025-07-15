d3931e963bb1e1b8ab7aeb2fc0ac84613d6d7eb3
RocioVera <rocioveradaniela@gmail.com>
2025-07-15 18:34:02 -0300

    Desarrollo del recycler view en stickerAdapter y su layout

--- app/src/main/java/com/example/mydinostickerscollection2/CollectionActivity.kt
+++ app/src/main/java/com/example/mydinostickerscollection2/CollectionActivity.kt
@@ -1,9 +1,13 @@
 package com.example.mydinostickerscollection2
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mydinostickerscollection2.data.User
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.mydinostickerscollection2.adapter.StickersAdapter
+import com.example.mydinostickerscollection2.data.Sticker
 import com.example.mydinostickerscollection2.databinding.ActivityCollectionBinding
-import com.example.mydinostickerscollection2.repositories.UserRepository
+import com.example.mydinostickerscollection2.repositories.StickersRepository
+
 
 class CollectionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCollectionBinding
@@ -12,8 +16,20 @@ class CollectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCollectionBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val nickname = intent.extras?.getString("USER_NICKNAME").orEmpty()
-        val password = intent.extras?.getString("USER_PASSWORD").orEmpty()
-        val usuario: User? = UserRepository.login(nickname, password)
+        setUpRecyclerView()
     }
+
+    private fun setUpRecyclerView(){
+        val selectStickerClickLister = {
+                sticker: Sticker ->
+            Toast.makeText(this, "Seleccionaste al sticker", Toast.LENGTH_LONG).show()
+        }
+        //Este es un ejemplo.No esta desarrollado
+        binding.textView2.text = buildString {  append("Coleccionadas: ${StickersRepository.getStickers().size} \n Repetidas: 6 \n ¡Te faltan 6 figuritas más para completar tu colección!") }
+        //Este inicializa el adapter
+        binding.recyclerViewStickers.adapter = StickersAdapter(StickersRepository.getStickers(),selectStickerClickLister )
+       //Este muestra los items en 2
+        binding.recyclerViewStickers.layoutManager = GridLayoutManager(this, 2)
+    }
+
 }
--- app/src/main/java/com/example/mydinostickerscollection2/adapter/StickersAdapter.kt
+++ app/src/main/java/com/example/mydinostickerscollection2/adapter/StickersAdapter.kt
@@ -1,4 +1,37 @@
 package com.example.mydinostickerscollection2.adapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mydinostickerscollection2.data.Sticker
+import com.example.mydinostickerscollection2.databinding.ItemStickerBinding
+import com.squareup.picasso.Picasso
 
-class StickersAdapter {
+class StickersAdapter(
+    private val stickers: List<Sticker>,
+    private val selectStickerClickLister: (Sticker) -> Unit
+) : RecyclerView.Adapter<StickersAdapter.StickersViewHolder>() {
+
+    class StickersViewHolder(val binding: ItemStickerBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StickersViewHolder {
+        val clientBinding =
+            ItemStickerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StickersViewHolder(clientBinding)
+    }
+
+    override fun onBindViewHolder(holder: StickersViewHolder, position: Int) {
+        val sticker = stickers[position]
+        holder.binding.tvStickerDescription.text = sticker.description
+        holder.binding.tvStickerName.text = sticker.name
+        Picasso.get().load(sticker.picture).into(holder.binding.ivStickerImage)
+        holder.binding.tvStickerExtraData.text = buildString { append("${sticker.height}M                  ${sticker.weight} \n ALTURA            PESO") }
+        holder.binding.stickerCard.setOnClickListener {
+            selectStickerClickLister(sticker)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return stickers.size
+    }
 }
